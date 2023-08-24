02790f110c47024fb728c51d930ca8ebfc6754e9
Torham <TorhamDev@gmail.com>
2023-08-24 18:09:35 +0330

    Run it on 0.0.0.0

--- dockerized_flask_simple/main.py
+++ dockerized_flask_simple/main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return "<h1>Hello Server 1!</h1>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
